8786e329cbbf291ae1cd0aba72624f02283e9268
kajhemmingsen <kaj.hemmingsen.85@gmail.com>
2021-02-15 21:29:33 -0500

    just splitting the front and back end repos

---
+++ smartbrain/src/Components/Navigation/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+    if(isSignedIn) {
+        return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p onClick={() =>onRouteChange('signout')} className='f3 link dim black underlined pa3 pointer'>Sign Out</p>
+        </nav>
+        )
+    } else {
+        return (
+            <div>
+                <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                    <p onClick={() =>onRouteChange('signin')} className='f3 link dim black underlined pa3 pointer'>Sign In</p>
+                </nav>
+                <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                    <p onClick={() =>onRouteChange('register')} className='f3 link dim black underlined pa3 pointer'>Register</p>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Navigation;
---
+++ smartbrain/src/Components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Rank = ({name, entries}) => {
+    
+    return (
+        <div>
+            <div className='white f3'>
+                {`${name}, you have submitted this many images: `}
+            </div>
+            <div className='white f1'>
+                {entries}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
